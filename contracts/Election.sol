cf1d0d71a283949ee38372a7ee4e875bbb386b07
Schartz Rehan <rehan.qt@gmail.com>
2019-01-24 18:46:21 +0530

    Initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+	// Read Candidate
+	string public candidate;
+
+	// Constructor
+	constructor() public {
+		// state variable
+		candidate = "Candidate 1";
+	}
+
+}
