3a6942b79a5e56daec77b4dcafff2564bc909b2c
Schartz Rehan <rehan.qt@gmail.com>
2019-01-26 16:31:18 +0530

    Add checks for invalid candidate and double voting

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -30,6 +30,12 @@ contract Election {
 	}
 
 	function vote(uint _candidateId) public  {
+		//require that voter has not voted before
+		require(!voters[msg.sender]);
+
+		// require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+
 		//record that voter has voted
 		voters[msg.sender] = true;
 
