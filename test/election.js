ecedc2c088305924694b354b8454d0d4c88130fa
Schartz Rehan <rehan.qt@gmail.com>
2019-01-26 16:04:01 +0530

    Add tests for voting action

--- test/election.js
+++ test/election.js
@@ -16,14 +16,30 @@ contract("Election", function(accounts) {
 			electionInstance = instance;
 			return electionInstance.candidates(1)
 		}).then(function(candidate){
-			assert.equal(candidate[0], 1, "contains the correct id")
-			assert.equal(candidate[1], "Candidate 1", "contains the correct name")
-			assert.equal(candidate[2], 0, "contains the correct voteCount")
+			assert.equal(candidate.id, 1, "contains the correct id")
+			assert.equal(candidate.name, "Candidate 1", "contains the correct name")
+			assert.equal(candidate.voteCount, 0, "contains the correct voteCount")
 			return electionInstance.candidates(2)
 		}).then(function(candidate){
-			assert.equal(candidate[0], 2, "contains the correct id")
-			assert.equal(candidate[1], "Candidate 2", "contains the correct name")
-			assert.equal(candidate[2], 0, "contains the correct voteCount")
+			assert.equal(candidate.id, 2, "contains the correct id")
+			assert.equal(candidate.name, "Candidate 2", "contains the correct name")
+			assert.equal(candidate.voteCount, 0, "contains the correct voteCount")
+		})
+	});
+
+	it("allows a voter to cast a vote", function(){
+		return Election.deployed().then(function(instance){
+			electionInstance = instance;
+			candidateId = 1;
+			return electionInstance.vote(candidateId, {from: accounts[0]});
+		}).then(function(receipt){
+			return electionInstance.voters(accounts[0]);
+		}).then(function(voted){
+			assert(voted, "the voter was marked voted");
+			return electionInstance.candidates(candidateId)
+		}).then(function(candidate){
+			var voteCount = candidate.voteCount;
+			assert.equal(voteCount, 1, "increments the cndidate's vote count");
 		})
 	});
 });
