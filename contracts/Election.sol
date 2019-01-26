5614fb8772cf327e3d71fa4aaf6d852abd744dcc
Schartz Rehan <rehan.qt@gmail.com>
2019-01-26 15:26:46 +0530

    Add front end and candidate listing

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,29 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-	// Read Candidate
-	string public candidate;
+	// Model a candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	//store candidates
+	mapping(uint => Candidate) public candidates;
+
+	//store candidate count
+	uint public candidatesCount;
 
 	// Constructor
 	constructor() public {
 		// state variable
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string memory _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 
 }
